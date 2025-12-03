d82247dfe72fc2f8d5b061e175abec9d3fc39803
hepengju <he_pengju@163.com>
2025-12-03 17:04:10 +0800

    窗口状态记录去掉，还是默认的大小好看，后期考虑添加的设置中

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,7 +13,8 @@ pub fn run() {
         .expect("Failed to install rustls crypto provider");
 
     tauri::Builder::default()
-        .plugin(tauri_plugin_window_state::Builder::new().build())
+        // 窗口状态插件暂时注释
+        // .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
