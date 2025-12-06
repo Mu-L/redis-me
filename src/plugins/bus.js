4b49009539d7884c660ab296e2cf3274cb73b354
hepengju <he_pengju@163.com>
2025-12-06 22:46:02 +0800

    删除多余的bus, pinia, directive

--- src/plugins/bus.js
+++
@@ -1,9 +0,0 @@
-import {bus} from '@/utils/util.js'
-
-// 安装给全局实例使用
-export default function (app) {
-  // 添加全局事件总线
-  // on监听, emit发生
-  // 参考: https://www.npmjs.com/package/mitt
-  app.config.globalProperties.$bus = bus
-}
