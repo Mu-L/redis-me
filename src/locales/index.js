61e91a288d816abc94d9efc7d511081374f6815d
hepengju <he_pengju@163.com>
2025-12-06 23:59:56 +0800

    i18n关闭html告警

--- src/locales/index.js
+++ src/locales/index.js
@@ -8,7 +8,9 @@ const i18n = createI18n({
   globalInjection: false, // 全局模式，可以直接使用 $t, 本应用统一使用t函数
   locale: 'en',
   fallbackLocale: 'en',
-  messages: {zhCN, en}
+  messages: {zhCN, en},
+
+  warnHtmlInMessage: false // 关闭html警告
 })
 
 export default i18n
