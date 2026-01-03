31d817ba168cdda92b4d5342ec04fd5da96f8e54
hepengju <he_pengju@163.com>
2026-01-03 11:26:26 +0800

    docs: 搜素i18n

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -14,7 +14,28 @@ export default defineConfig({
       {icon: 'github', link: 'https://github.com/hepengju/redis-me'},
     ],
     search: {
-      provider: 'local'
+      provider: 'local',
+      options: {
+        locales: {
+          zh: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭'
+                }
+              }
+            }
+          }
+        }
+      }
     },
   },
 
