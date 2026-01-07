56e2f19b455b8abe880951a7c5b60d0feb2405a8
hepengju <he_pengju@163.com>
2026-01-07 22:24:11 +0800

    升级依赖，修改默认端口1420为2222

--- vite.config.js
+++ vite.config.js
@@ -39,14 +39,14 @@ export default defineConfig({
     clearScreen: false,
     // 2. tauri expects a fixed port, fail if that port is not available
     server: {
-        port: 1420,
+        port: 2222,
         strictPort: true,
         host: host || false,
         hmr: host
           ? {
               protocol: "ws",
               host,
-              port: 1421,
+              port: 2221,
           }
           : undefined,
         watch: {
