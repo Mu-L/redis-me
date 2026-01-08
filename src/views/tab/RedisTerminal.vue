af0bc789bd126077e82c81ee7af206156964511d
hepengju <he_pengju@163.com>
2026-01-08 16:40:47 +0800

    Xterm ==> XTerminal，简化很多代码，且支持中文退格及多行命令等

--- src/views/tab/RedisTerminal.vue
+++ src/views/tab/RedisTerminal.vue
@@ -3,47 +3,30 @@ import NodeList from '../ext/NodeList.vue'
 import {meInvoke} from '@/utils/util.js'
 import MeIcon from '@/components/MeIcon.vue'
 import {useI18n} from 'vue-i18n'
-import {useDark} from '@vueuse/core'
 
 const { t } = useI18n()
 // 共享数据
 const share = inject('share')
 const canEdit = computed(() => !share.readonly)
 
-const isDark = useDark()
-const color  = computed(() => isDark.value ? '32m' : '34m')
-
 const autoBroadcast = ref(true)
 const node  = ref('')
 const hint = computed(() => t('redisTerminal.hint'))
-const prefix = computed(() => node.value ? `\x1B[1;3;${color.value}${node.value}> \x1B[0m` : `\x1B[1;3;${color.value}$ \x1B[0m`)
-const welcome = computed(() => t('redisTerminal.welcome', {RedisME: `\x1B[1;3;${color.value}RedisME\x1B[0m`}))
+const prefix = computed(() => `<span style="color: var(--el-color-primary)">${node.value ? node.value + '> ' : '$ '}</span>`)
+const welcome = computed(() => t('redisTerminal.welcome', {RedisME: `<span style="color: var(--el-color-primary)">RedisME</span>`}))
 
 // 定制化执行命令
 async function execCommand(command) {
   if (!canEdit.value) {
-    return '\x1B[1;3;' + color.value + t('redisTerminal.readonlyHint') + '\x1B[0m'
+    return `<span style="color: var(--el-color-error)">${t('redisTerminal.readonlyHint')}</span>`
   }
 
   try {
-    const param = {
-      command, node: node.value, autoBroadcast: autoBroadcast.value
-    }
+    const param = {command, node: node.value, autoBroadcast: autoBroadcast.value}
     const data = await meInvoke('execute_command', {id: share.conn.id, param}, false)
-    const result = replaceEnter(data)
-    return `\x1b[35;1m${result}\x1b[0m`
+    return `<span style="color: var(--el-color-success)">${data}</span`
   } catch (e) {
-    return `\x1b[31;1m(error) ${e}\x1b[0m`
-  }
-}
-
-function replaceEnter(data) {
-  if (data == null || data == undefined) {
-    return ''
-  } else if (Array.isArray(data)) {
-    return data.map(d => replaceEnter(d)).join('\r\n')
-  } else {
-    return data?.split('\n').join('\r\n')
+    return `<span style="color: var(--el-color-error)">(error) ${e}</span`
   }
 }
 </script>
