f0a7225dade2555ad14af322d13fa980d0c4d02d
hepengju <he_pengju@163.com>
2026-01-17 15:50:05 +0800

    isAppStore判断修改为依赖文件是否存在

--- src/plugins/tauri.js
+++ src/plugins/tauri.js
@@ -1,8 +1,9 @@
 import {Window} from '@tauri-apps/api/window'
 import {locale} from '@tauri-apps/plugin-os'
 import {LazyStore} from '@tauri-apps/plugin-store'
-import {appConfigDir, appDataDir, appLogDir} from '@tauri-apps/api/path'
+import {appConfigDir, appDataDir, appLocalDataDir, appLogDir, BaseDirectory} from '@tauri-apps/api/path'
 import {meLog} from '@/utils/util.js'
+import {exists} from '@tauri-apps/plugin-fs'
 
 // 打包后关闭右键菜单
 if (import.meta.env.PROD) {
@@ -18,13 +19,18 @@ meLog('系统主题:', systemTheme, '系统语言:', systemLanguage)
 const configDir = await appConfigDir()
 const dataDir = await appDataDir()
 const logDir = await appLogDir()
+const localDataDir = await appLocalDataDir()
 meLog('配置目录:', configDir) // C:\Users\he_pe\AppData\Roaming\com.hepengju.redis
-meLog('数据目录:', dataDir)
 meLog('日志目录:', logDir)
+meLog('数据目录:', dataDir)
+meLog('本地数据目录:', localDataDir)
 
 // 应用商店里面的更新依赖应用商店自身的更新机制
 // 微软应用商店示例: C:\Program Files\WindowsApps\hepengju.RedisME_1.2.0.0_x64__v2a7j12f6a642\VFS\Local AppData\RedisME
-const isAppStore = configDir.includes('WindowsApps')
+// const isAppStore = configDir.includes('WindowsApps')
+// 实测VFS文件系统中读取不到原始目录，修改判断方式为: resources目录下是否存在appStore.me文件
+const isAppStore = await exists('isAppStore.me', {baseDir: BaseDirectory.AppLocalData})
+meLog('应用商店应用:', isAppStore, '(isAppStore.me)')
 
 // 存储及初始化数据读取
 const store = new LazyStore('store.json')
