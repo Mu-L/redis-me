232a423f02169c87fae54dc77b5df55cf733d778
hepengju <he_pengju@163.com>
2026-01-21 22:15:24 +0800

    改用vue-chart.js

--- src/views/tab/RedisChart.vue
+++ src/views/tab/RedisChart.vue
@@ -1,13 +1,58 @@
-<script setup>
-import {useI18n} from 'vue-i18n'
+<template></template>
+<!--<script setup>-->
+<!--import {useI18n} from 'vue-i18n'-->
+<!--// import {VueUiXy} from 'vue-data-ui/vue-ui-xy'-->
+<!--// import 'vue-data-ui/style.css'-->
 
-const { t } = useI18n()
-</script>
+<!--const {t} = useI18n()-->
+<!--const config = ref({-->
+<!--  chart: {-->
+<!--    height: 200,-->
+<!--    zoom: {-->
+<!--      show: false-->
+<!--    },-->
+<!--    legend: {-->
+<!--      show: false-->
+<!--    },-->
+<!--    title: {-->
+<!--      show: true,-->
+<!--      text: '键数量'-->
+<!--    }-->
+<!--  }-->
+<!--})-->
+<!--const dataset = ref([-->
+<!--  {-->
+<!--    "series": [-->
+<!--      0,-->
+<!--      1,-->
+<!--      2,-->
+<!--      3,-->
+<!--      5,-->
+<!--      8,-->
+<!--      13,-->
+<!--      21,-->
+<!--      34,-->
+<!--      55,-->
+<!--      89,-->
+<!--      144-->
+<!--    ],-->
+<!--    "type": "line",-->
+<!--  }-->
+<!--])-->
+<!--</script>-->
 
-<template>
-  Chart图表
-</template>
+<!--<template>-->
+<!--  <div class="redis-chart">-->
+<!--    <VueUiXy ref="xyChart" :config="config" :dataset="dataset"/>-->
+<!--    <VueUiXy ref="xyChart" :config="config" :dataset="dataset"/>-->
+<!--    <VueUiXy ref="xyChart" :config="config" :dataset="dataset"/>-->
+<!--    <VueUiXy ref="xyChart" :config="config" :dataset="dataset"/>-->
+<!--  </div>-->
+<!--</template>-->
 
-<style scoped lang="scss">
-
-</style>
+<!--<style scoped lang="scss">-->
+<!--.redis-chart {-->
+<!--  height: 100%;-->
+<!--  border: 2px solid red;-->
+<!--}-->
+<!--</style>-->
