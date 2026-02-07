20a5748ffa584333ee916b87fd153f2e023236ca
hepengju <he_pengju@163.com>
2026-02-07 10:12:02 +0800

    引入 vue-web-terminal

--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ import setupSvgIcon from '@/plugins/icon.js'
 import setupMe from '@/plugins/me.js'
 import setupTauri from '@/plugins/tauri.js'
 import setupI18n from '@/plugins/i18n.js'
+import setupTernimal from '@/plugins/ternimal.js'
 
 const app = createApp(App)
+
 setupElementPlus(app)
 setupSvgIcon(app)
 setupMe(app)
 setupTauri(app)
 setupI18n(app)
+setupTernimal(app)
+
 app.mount('#app')
---
+++ src/plugins/ternimal.js
@@ -0,0 +1,19 @@
+import {createTerminal} from 'vue-web-terminal'
+
+const terminal = createTerminal()
+
+// 配置本地存储名
+// default is 'terminal'
+// terminal.configStoreName('terminal')
+
+// 配置每个Terminal实例记忆历史指令的最大数量
+// default is 100
+// terminal.configMaxStoredCommandCountPerInstance(100)
+
+// 配置自定义主题
+// import customTheme from '/your-style-dir/terminal-custom-theme1.css?inline'
+// terminal.configTheme('my-custom-theme', customTheme)
+
+export default function (app) {
+  app.use(terminal)
+}
