42abcb3ec26d6e6dea6a8089350301539eff2aa3
hepengju <he_pengju@163.com>
2026-03-06 22:51:05 +0800

    info: 官网跳转支持多个

---
+++ src/components/MeWebsite.vue
@@ -0,0 +1,52 @@
+<script setup>
+// 跳转到官网: Redis中英文/Valkey中英文
+import {useI18n} from 'vue-i18n'
+import {openUrl} from '@tauri-apps/plugin-opener'
+import {isZh, meOk} from '@/utils/util.js'
+
+const {to} = defineProps({
+  to: {type: String, required: true},
+  placement: {type: String, default: 'right'},
+  marginLeft: {type: String, default: '10px'},
+})
+
+const { t } = useI18n()
+
+const websize = {
+  'redis': 'https://redis.io',
+  'redis-zh': 'https://redis.ac.cn',
+  'valkey': 'https://valkey.io',
+  'valkey-zh': 'https://valkey.cn',
+}
+
+const info = {
+  'redis': '/docs/latest/commands/info/',
+  'redis-zh': '/docs/latest/commands/info/',
+  'valkey': '/commands/info/',
+  'valkey-zh': '/commands/info/',
+}
+
+function handleCommand(command) {
+  if (to === 'info') {
+    openUrl(websize[command] + info[command])
+  } else {
+    meOk(`TODO: ${command}`)
+  }
+}
+
+</script>
+
+<template>
+  <el-dropdown @command="handleCommand"
+               trigger="hover" :placement :style="{marginLeft}">
+    <me-icon icon="me-icon-link" style="font-size: 14px; color: var(--el-color-success)"/>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item command="redis"> <me-icon icon="me-icon-redis" name="Redis"/></el-dropdown-item>
+        <el-dropdown-item command="valkey"><me-icon icon="me-icon-valkey" name="Valkey"/></el-dropdown-item>
+        <el-dropdown-item command="redis-zh" v-if="isZh"><me-icon icon="me-icon-redis" name="Redis 中文"/></el-dropdown-item>
+        <el-dropdown-item command="valkey-zh" v-if="isZh"><me-icon icon="me-icon-valkey" name="Valkey 中文"/></el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
